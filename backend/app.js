72f8ef0e058095101067363021506f4f2c176013
kongo bola emmanouch <emchkongo@gmail.com>
2026-04-24 00:20:27 +0200

    fix: update cors for vercel

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
-// Remplace ton bloc CORS actuel par celui-ci pour être sûr
-// Configuration CORS simplifiée et efficace
-app.use(
-  cors({
-    origin: ["https://scolaris-tau.vercel.app", "http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true
-  })
-);
+const cors = require("cors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
